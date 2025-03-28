5805e00c4c9ebdb53cb24dd84e2b6e205cd5842a
handler <yishengxie@163.com>
2025-03-28 08:57:10 +0800

    feat:npm

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "liko",
+      formats: ["cjs", "es"],
+      fileName: (format) => `liko.${format}.js`,
+    },
+    sourcemap: true,
     rollupOptions: {
-      input: {
-        default: "index.html"
+      external: ["@webgpu/types", "planck"],
+      output: {
+        globals: {
+          planck: "planck",
+        },
       },
     },
-  }
+  },
 });
