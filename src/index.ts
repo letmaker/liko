87a0f08a2704b576c5dad914893e3e3d4900697f
handler <yishengxie@163.com>
2025-06-03 22:41:34 +0800

    feat: 更新版本至 0.2.16，新增 DirtyType 导出以增强事件类型管理

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { EventType } from './const';
+export { EventType ,DirtyType } from './const';
 export * as register from './utils/register';
 export * as utils from './utils/utils';
 export { system } from './utils/system';
@@ -16,7 +16,7 @@ export { Texture } from './resource/texture';
 export { TextureBuffer } from './render/buffer/texture-buffer';
 export { Canvas } from './nodes/canvas';
 export { Container } from './nodes/container';
-export { LikoNode, type INodeData, type IScriptData, type IFilterData } from './nodes/node';
+export { LikoNode, type INodeData, type IScriptData, type IFilterData} from './nodes/node';
 export { Scene } from './nodes/scene';
 export { Sprite } from './nodes/sprite';
 export { AnimatedSprite } from './nodes/animated-sprite';
